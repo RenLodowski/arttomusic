dc1d34eb9bf69042cea8bfc060f9254a35fa9182
RenLodowski <lodowsra01@sunyjcc.edu>
2023-04-20 16:12:56 -0400

    Update art2music.py
    basic task layout

--- art2music.py
+++ art2music.py
@@ -5,3 +5,19 @@
 # verson - 0.1a
 #=================================================================================
 
+
+#import libraries
+#keep track of settings
+#allow user input
+
+#check for upload or sample
+#check if image is loaded
+#save into temp file
+#view image
+#get HSV values
+#grab H from HSV for each pixel
+#convert H to frequency
+#save song
+#make effected version
+#save final song
+#save midi version
